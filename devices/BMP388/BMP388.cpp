7b7417271b85892d7a54b7a8b1f3513993f3d168
Michael Wigham <michael@wigham.net>
2024-03-18 19:53:37 +0000

    Add BMP388 initial support

---
+++ devices/BMP388/BMP388.cpp
@@ -0,0 +1,290 @@
+/**
+ * @file BMP388.cpp
+ * @author Michael Wigham (michael@wigham.net)
+ * @brief // TODO:
+ * @date 2024-03-18
+ *
+ * @copyright Copyright (c) 2024
+ */
+
+#include <efc_devices/BMP388.hpp>
+
+BMP388::BMP388(uint8_t address) : I2CDevice(address) {
+    /* Soft reset */
+    write_reg(BMP388_REG_CMD, 0xB6);
+
+    ets_delay_us(10000);
+
+    auto [chip_id] = read_registers<uint8_t, 1>(BMP388_REG_CHIP_ID);
+    if (chip_id != 0x50) {
+        // Chip id wrong
+    }
+
+    /* Setup */
+    auto [osr_reg, odr_reg, pwr_ctrl_reg] = read_registers<uint8_t, 3>(0x1B);
+    write_reg(BMP388_REG_PWR_CTRL, pwr_ctrl_reg | BMP388_MODE_NORMAL_MODE | 0b00000011);
+    set_pressure_oversampling(BMP388_OVERSAMPLING_x8);
+    set_temperature_oversampling(BMP388_OVERSAMPLING_x1);
+    set_odr(BMP388_ODR_50_HZ);
+
+    auto [config_reg] = read_registers<uint8_t, 1>(BMP388_REG_CONFIG);
+    write_reg(BMP388_REG_CONFIG, config_reg | 0b00000100);
+
+    // Get Calibration data
+    auto [t1] = read_registers<uint16_t, 1>(BMP388_REG_NVM_PAR_T1_L);
+    m_t1 = t1;
+    auto [t2] = read_registers<uint16_t, 1>(BMP388_REG_NVM_PAR_T2_L);
+    m_t2 = t2;
+    auto [t3] = read_registers<uint8_t, 1>(BMP388_REG_NVM_PAR_T3);
+    m_t3 = (int8_t)t3;
+    auto [p1] = read_registers<uint16_t, 1>(BMP388_REG_NVM_PAR_P1_L);
+    m_p1 = p1;
+    auto [p2] = read_registers<uint16_t, 1>(BMP388_REG_NVM_PAR_P2_L);
+    m_p2 = p2;
+    auto [p3] = read_registers<uint8_t, 1>(BMP388_REG_NVM_PAR_P3);
+    m_p3 = (int8_t)p3;
+    auto [p4] = read_registers<uint8_t, 1>(BMP388_REG_NVM_PAR_P4);
+    m_p4 = (int8_t)p4;
+    auto [p5] = read_registers<uint16_t, 1>(BMP388_REG_NVM_PAR_P5_L);
+    m_p5 = p5;
+    auto [p6] = read_registers<uint16_t, 1>(BMP388_REG_NVM_PAR_P6_L);
+    m_p6 = p6;
+    auto [p7] = read_registers<uint8_t, 1>(BMP388_REG_NVM_PAR_P7);
+    m_p7 = (int8_t)p7;
+    auto [p8] = read_registers<uint8_t, 1>(BMP388_REG_NVM_PAR_P8);
+    m_p8 = (int8_t)p8;
+    auto [p9] = read_registers<uint16_t, 1>(BMP388_REG_NVM_PAR_P9_L);
+    m_p9 = p9;
+    auto [p10] = read_registers<uint8_t, 1>(BMP388_REG_NVM_PAR_P10);
+    m_p10 = (int8_t)p10;
+    auto [p11] = read_registers<uint8_t, 1>(BMP388_REG_NVM_PAR_P11);
+    m_p11 = (int8_t)p11;
+};
+
+bmp388_odr_t BMP388::get_odr() {
+    auto [odr_reg] = read_registers<uint8_t, 1>(BMP388_REG_ODR);
+
+    return (bmp388_odr_t)((odr_reg >> 0) & 31);
+}
+
+void BMP388::set_odr(bmp388_odr_t odr) {
+    auto [odr_reg] = read_registers<uint8_t, 1>(BMP388_REG_ODR);
+
+    odr_reg &= ~(0b00011111 << 0);
+    odr_reg |= odr << 0;
+    write_reg(BMP388_REG_ODR, odr_reg);
+}
+
+bmp388_oversampling_t BMP388::get_pressure_oversampling() {
+    auto [osr_reg] = read_registers<uint8_t, 1>(BMP388_REG_OSR);
+
+    return (bmp388_oversampling_t)((osr_reg >> 0) & 0x07);
+}
+
+void BMP388::set_pressure_oversampling(bmp388_oversampling_t oversampling) {
+    auto [prev_osr_reg] = read_registers<uint8_t, 1>(BMP388_REG_OSR);
+
+    prev_osr_reg &= ~(7 << 0);
+    prev_osr_reg |= oversampling << 0;
+    write_reg(BMP388_REG_OSR, prev_osr_reg);
+}
+
+bmp388_oversampling_t BMP388::get_temperature_oversampling() {
+    auto [osr_reg] = read_registers<uint8_t, 1>(BMP388_REG_OSR);
+
+    return (bmp388_oversampling_t)((osr_reg >> 3) & 0x07);
+}
+
+void BMP388::set_temperature_oversampling(bmp388_oversampling_t oversampling) {
+    auto [prev_osr_reg] = read_registers<uint8_t, 1>(BMP388_REG_OSR);
+
+    prev_osr_reg &= ~(7 << 3);
+    prev_osr_reg |= oversampling << 3;
+    write_reg(BMP388_REG_OSR, prev_osr_reg);
+}
+
+float BMP388::get_temperature() {
+    auto [fifo_cfg1] = read_registers<uint8_t, 1>(BMP388_REG_FIFO_CONFIG_1);
+    if (((fifo_cfg1 & BIT(0)) != 0) || ((fifo_cfg1 & BIT(4)) != 0)) {
+        ESP_LOGE("BMP388", "fifo mode disabled or no temperature data is stored");
+        return 0;
+    }
+    auto [pwr_ctrl_reg] = read_registers<uint8_t, 1>(BMP388_REG_PWR_CTRL);
+    if (((pwr_ctrl_reg >> 4) & 0x03) == 0x03) {
+        auto [status_reg] = read_registers<uint8_t, 1>(BMP388_REG_STATUS);
+        if ((status_reg & (1 << 6)) != 0) {
+            int64_t output;
+
+            auto [data3_reg, data4_reg, data5_reg] = read_registers<uint8_t, 3>(BMP388_REG_DATA_3);
+            uint32_t raw_data = (uint32_t)data5_reg << 16 | (uint32_t)data4_reg << 8 | data3_reg;
+            output = _comp_temperature(raw_data);
+            return (float)((double)output / 100.0);
+        }
+        ESP_LOGI("BMP388", "Temperature data not ready to be read");
+        return 0;
+    } else if (((fifo_cfg1 >> 4) & 0x03) == 0x00) {
+        uint16_t cnt = 5000;
+
+        pwr_ctrl_reg &= ~(0x03 << 4); /* clear 4-5 bits */
+        pwr_ctrl_reg |= 0x01 << 4;    /* set bit 4 */
+        write_reg(BMP388_REG_PWR_CTRL, pwr_ctrl_reg);
+
+        while (1) {
+            auto [status_reg] = read_registers<uint8_t, 1>(BMP388_REG_STATUS);
+            if ((status_reg & (1 << 6)) != 0) {
+                int64_t output;
+
+                auto [data3_reg, data4_reg, data5_reg] = read_registers<uint8_t, 3>(BMP388_REG_DATA_3);
+                uint32_t raw_data = (uint32_t)data5_reg << 16 | (uint32_t)data4_reg << 8 | data3_reg;
+                output = _comp_temperature(raw_data);
+                return (float)((double)output / 100.0);
+            } else {
+                if (cnt != 0) {
+                    cnt--;
+                    ets_delay_us(1000);
+                    continue;
+                }
+                ESP_LOGI("BMP388", "Temperature data not ready to be read");
+                return 0;
+            }
+        }
+    } else {
+        ESP_LOGI("BMP388", "mode is invalid");
+        return 0;
+    }
+}
+
+float BMP388::get_pressure() {
+    uint32_t temperature_yaw;
+
+    auto [fifo_cfg1] = read_registers<uint8_t, 1>(BMP388_REG_FIFO_CONFIG_1);
+    if (((fifo_cfg1 & BIT(0)) != 0) || ((fifo_cfg1 & BIT(4)) != 0) || ((fifo_cfg1 & BIT(3)) != 0)) {
+        ESP_LOGE("BMP388", "fifo mode disabled or no temperature or pressure data is stored");
+        return 0;
+    }
+    auto [pwr_ctrl_reg] = read_registers<uint8_t, 1>(BMP388_REG_PWR_CTRL);
+    if (((pwr_ctrl_reg >> 4) & 0x03) == 0x03) {
+        auto [status_reg] = read_registers<uint8_t, 1>(BMP388_REG_STATUS);
+        if ((status_reg & (1 << 6)) != 0) {
+            auto [data3_reg, data4_reg, data5_reg] = read_registers<uint8_t, 3>(BMP388_REG_DATA_3);
+            temperature_yaw = (uint32_t)data5_reg << 16 | (uint32_t)data4_reg << 8 | data3_reg;
+            (void)_comp_temperature(temperature_yaw);
+        } else {
+            ESP_LOGI("BMP388", "Temperature data not ready to be read");
+            return 0;
+        }
+        if ((status_reg & (1 << 5)) != 0) {
+            int64_t output;
+
+            auto [data0_reg, data1_reg, data2_reg] = read_registers<uint8_t, 3>(BMP388_REG_DATA_0);
+            ESP_LOGI("BMP388", "PRESS data %d %d %d", data2_reg, data1_reg, data0_reg);
+            uint32_t data = (uint32_t)data2_reg << 16 | (uint32_t)data1_reg << 8 | data0_reg;
+            output = _comp_pressure(data);
+            return (float)((double)output / 100.0);
+        } else {
+            ESP_LOGI("BMP388", "Pressure data not ready to be read");
+            return 0;
+        }
+    } else if (((pwr_ctrl_reg >> 4) & 0x03) == 0x00) {
+        uint16_t cnt = 5000;
+
+        pwr_ctrl_reg &= ~(0x03 << 4);
+        pwr_ctrl_reg |= 0x01 << 4;
+        write_reg(BMP388_REG_PWR_CTRL, pwr_ctrl_reg);
+
+        while (1) {
+            auto [status_reg] = read_registers<uint8_t, 1>(BMP388_REG_STATUS);
+            if ((status_reg & (1 << 6)) != 0) {
+                auto [data3_reg, data4_reg, data5_reg] = read_registers<uint8_t, 3>(BMP388_REG_DATA_3);
+                temperature_yaw = (uint32_t)data5_reg << 16 | (uint32_t)data4_reg << 8 | data3_reg;
+                (void)_comp_temperature(temperature_yaw);
+            } else {
+                if (cnt != 0) {
+                    cnt--;
+                    ets_delay_us(1000);
+                    continue;
+                }
+                ESP_LOGI("BMP388", "Temperature data not ready to be read");
+                return 0;
+            }
+            cnt = 5000;
+            if ((status_reg & (1 << 5)) != 0) {
+                int64_t output;
+
+                auto [data0_reg, data1_reg, data2_reg] = read_registers<uint8_t, 3>(BMP388_REG_DATA_0);
+                uint32_t data = (uint32_t)data2_reg << 16 | (uint32_t)data1_reg << 8 | data0_reg;
+                output = _comp_pressure(data);
+                return (float)((double)output / 100.0);
+            } else {
+                if (cnt != 0) {
+                    cnt--;
+                    ets_delay_us(1000);
+                    continue;
+                }
+                ESP_LOGI("BMP388", "Temperature data not ready to be read");
+                return 0;
+            }
+        }
+    }
+    ESP_LOGI("BMP388", "mode is invalid");
+    return 0;
+}
+
+int64_t BMP388::_comp_temperature(uint32_t data) {
+    uint64_t pd1;
+    uint64_t pd2;
+    uint64_t pd3;
+    int64_t pd4;
+    int64_t pd5;
+    int64_t pd6;
+    int64_t ct;
+
+    /* calculate compensate temperature */
+    pd1 = (uint64_t)(data - (256 * (uint64_t)(m_t1)));
+    pd2 = (uint64_t)(m_t2 * pd1);
+    pd3 = (uint64_t)(pd1 * pd1);
+    pd4 = (int64_t)(((int64_t)pd3) * ((int64_t)m_t3));
+    pd5 = ((int64_t)(((int64_t)pd2) * 262144) + (int64_t)pd4);
+    pd6 = (int64_t)(((int64_t)pd5) / 4294967296U);
+    m_t_fine = pd6;
+    ct = (int64_t)((pd6 * 25) / 16384);
+
+    return ct;
+}
+
+int64_t BMP388::_comp_pressure(uint32_t data) {
+    int64_t pd1;
+    int64_t pd2;
+    int64_t pd3;
+    int64_t pd4;
+    int64_t pd5;
+    int64_t pd6;
+    int64_t offset;
+    int64_t sensitivity;
+    uint64_t comp_press;
+
+    /* calculate compensate pressure */
+    pd1 = m_t_fine * m_t_fine;
+    pd2 = pd1 / 64;
+    pd3 = (pd2 * m_t_fine) / 256;
+    pd4 = (m_p8 * pd3) / 32;
+    pd5 = (m_p7 * pd1) * 16;
+    pd6 = (m_p6 * m_t_fine) * 4194304;
+    offset = (int64_t)((int64_t)(m_p5) * (int64_t)140737488355328U) + pd4 + pd5 + pd6;
+    pd2 = (((int64_t)m_p4) * pd3) / 32;
+    pd4 = (m_p3 * pd1) * 4;
+    pd5 = ((int64_t)(m_p2)-16384) * ((int64_t)m_t_fine) * 2097152;
+    sensitivity = (((int64_t)(m_p1)-16384) * (int64_t)70368744177664U) + pd2 + pd4 + pd5;
+    pd1 = (sensitivity / 16777216) * data;
+    pd2 = (int64_t)(m_p10) * (int64_t)(m_t_fine);
+    pd3 = pd2 + (65536 * (int64_t)(m_p9));
+    pd4 = (pd3 * data) / 8192;
+    pd5 = (pd4 * data) / 512;
+    pd6 = (int64_t)((uint64_t)data * (uint64_t)data);
+    pd2 = ((int64_t)(m_p11) * (int64_t)(pd6)) / 65536;
+    pd3 = (pd2 * data) / 128;
+    pd4 = (offset / 4) + pd1 + pd5 + pd3;
+    comp_press = (((uint64_t)pd4 * 25) / (uint64_t)1099511627776U);
+
+    return comp_press;
+}
--- devices/BMP388/include/efc_devices/BMP388.hpp
+++ devices/BMP388/include/efc_devices/BMP388.hpp
@@ -1,8 +1,9 @@
 /**
  * @file BMP388.hpp
  * @author Szymon Wlodarczyk (Szymon Wlodarczyk)
+ * @author Michael Wigham (michael@wigham.net)
  * @brief // TODO:
- * @date 2024-02-01
+ * @date 2024-03-18
  *
  * @copyright Copyright (c) 2024
  */
@@ -11,30 +12,136 @@
 #define EFC_BMP388_HPP
 
 #include <efc_drivers/i2c_device.hpp>
+#include <rom/ets_sys.h>
 
-#define BMP388_DEFAULT_ADRESS CONFIG_EFC_DEVICES_BMP388_SLAVE_ADDRESS
+#define BMP388_DEFAULT_ADDRESS CONFIG_EFC_DEVICES_BMP388_SLAVE_ADDRESS
+
+#define BMP388_REG_CMD 0x7E           /**< command register */
+#define BMP388_REG_NVM_PAR_T1_L 0x31  /**< NVM PAR T1 low register */
+#define BMP388_REG_NVM_PAR_T1_H 0x32  /**< NVM PAR T1 high register */
+#define BMP388_REG_NVM_PAR_T2_L 0x33  /**< NVM PAR T2 low register */
+#define BMP388_REG_NVM_PAR_T2_H 0x34  /**< NVM PAR T2 high register */
+#define BMP388_REG_NVM_PAR_T3 0x35    /**< NVM PAR T3 register */
+#define BMP388_REG_NVM_PAR_P1_L 0x36  /**< NVM PAR P1 low register */
+#define BMP388_REG_NVM_PAR_P1_H 0x37  /**< NVM PAR P1 high register */
+#define BMP388_REG_NVM_PAR_P2_L 0x38  /**< NVM PAR P2 low register */
+#define BMP388_REG_NVM_PAR_P2_H 0x39  /**< NVM PAR P2 high register */
+#define BMP388_REG_NVM_PAR_P3 0x3A    /**< NVM PAR P3 register */
+#define BMP388_REG_NVM_PAR_P4 0x3B    /**< NVM PAR P4 register */
+#define BMP388_REG_NVM_PAR_P5_L 0x3C  /**< NVM PAR P5 low register */
+#define BMP388_REG_NVM_PAR_P5_H 0x3D  /**< NVM PAR P5 high register */
+#define BMP388_REG_NVM_PAR_P6_L 0x3E  /**< NVM PAR P6 low register */
+#define BMP388_REG_NVM_PAR_P6_H 0x3F  /**< NVM PAR P6 high register */
+#define BMP388_REG_NVM_PAR_P7 0x40    /**< NVM PAR P7 register */
+#define BMP388_REG_NVM_PAR_P8 0x41    /**< NVM PAR P8 register */
+#define BMP388_REG_NVM_PAR_P9_L 0x42  /**< NVM PAR P9 low register */
+#define BMP388_REG_NVM_PAR_P9_H 0x43  /**< NVM PAR P9 high register */
+#define BMP388_REG_NVM_PAR_P10 0x44   /**< NVM PAR P10 register */
+#define BMP388_REG_NVM_PAR_P11 0x45   /**< NVM PAR P11 register */
+#define BMP388_REG_CONFIG 0x1F        /**< configure register */
+#define BMP388_REG_ODR 0x1D           /**< odr register */
+#define BMP388_REG_OSR 0x1C           /**< osr register */
+#define BMP388_REG_PWR_CTRL 0x1B      /**< power control register */
+#define BMP388_REG_IF_CONF 0x1A       /**< if configure register */
+#define BMP388_REG_INT_CTRL 0x19      /**< interrupt control register */
+#define BMP388_REG_FIFO_CONFIG_2 0x18 /**< fifo configure 2 register */
+#define BMP388_REG_FIFO_CONFIG_1 0x17 /**< fifo configure 1 register */
+#define BMP388_REG_FIFO_WTM_1 0x16    /**< fifo watermark 1 register */
+#define BMP388_REG_FIFO_WTM_0 0x15    /**< fifo watermark 2 register */
+#define BMP388_REG_FIFO_DATA 0x14     /**< fifo data register */
+#define BMP388_REG_FIFO_LENGTH_1 0x13 /**< fifo length 1 register */
+#define BMP388_REG_FIFO_LENGTH_0 0x12 /**< fifo length 0 register */
+#define BMP388_REG_INT_STATUS 0x11    /**< interrupt status register */
+#define BMP388_REG_EVENT 0x10         /**< event register */
+#define BMP388_REG_SENSORTIME_2 0x0E  /**< sensor time 2 register */
+#define BMP388_REG_SENSORTIME_1 0x0D  /**< sensor time 1 register */
+#define BMP388_REG_SENSORTIME_0 0x0C  /**< sensor time 0 register */
+#define BMP388_REG_DATA_5 0x09        /**< data 5 register - TEMP_MSB_23_16 */
+#define BMP388_REG_DATA_4 0x08        /**< data 4 register - TEMP_LSB_15_8 */
+#define BMP388_REG_DATA_3 0x07        /**< data 3 register - TEMP_XLSB_7_0 */
+#define BMP388_REG_DATA_2 0x06        /**< data 2 register - PRESS_MSB_23_16 */
+#define BMP388_REG_DATA_1 0x05        /**< data 1 register - PRESS_LSB_15_8 */
+#define BMP388_REG_DATA_0 0x04        /**< data 0 register - PRESS_XLSB_7_0 */
+#define BMP388_REG_STATUS 0x03        /**< status register */
+#define BMP388_REG_ERR_REG 0x02       /**< error register */
+#define BMP388_REG_CHIP_ID 0x00       /**< chip id register */
+
+typedef enum {
+    BMP388_OVERSAMPLING_x1 = 0x00,
+    BMP388_OVERSAMPLING_x2 = 0x01,
+    BMP388_OVERSAMPLING_x4 = 0x02,
+    BMP388_OVERSAMPLING_x8 = 0x03,
+    BMP388_OVERSAMPLING_x16 = 0x04,
+    BMP388_OVERSAMPLING_x32 = 0x05
+} bmp388_oversampling_t;
+
+typedef enum {
+    BMP388_MODE_SLEEP_MODE = 0b00000000,
+    BMP388_MODE_FORCED_MODE = 0b00010000,
+    BMP388_MODE_NORMAL_MODE = 0b00110000
+} bmp388_mode_t;
+
+typedef enum {
+    BMP388_ODR_200_HZ = 0x00,
+    BMP388_ODR_100_HZ = 0x01,
+    BMP388_ODR_50_HZ = 0x02,
+    BMP388_ODR_25_HZ = 0x03,
+    BMP388_ODR_12P5_HZ = 0x04,
+    BMP388_ODR_6P25_HZ = 0x05,
+    BMP388_ODR_3P1_HZ = 0x06,
+    BMP388_ODR_1P5_HZ = 0x07,
+    BMP388_ODR_0P78_HZ = 0x08,
+    BMP388_ODR_0P39_HZ = 0x09,
+    BMP388_ODR_0P2_HZ = 0x0A,
+    BMP388_ODR_0P1_HZ = 0x0B,
+    BMP388_ODR_0P05_HZ = 0x0C,
+    BMP388_ODR_0P02_HZ = 0x0D,
+    BMP388_ODR_0P01_HZ = 0x0E,
+    BMP388_ODR_0P006_HZ = 0x0F,
+    BMP388_ODR_0P003_HZ = 0x10,
+    BMP388_ODR_0P0015_HZ = 0x11
+} bmp388_odr_t;
 
 class BMP388 : I2CDevice {
 public:
-    BMP388(uint8_t address = BMP388_DEFAULT_ADRESS) : I2CDevice(address) {
-        /* Soft reset */
-        write_reg(0x7E, 0xB6);
+    BMP388(uint8_t address = BMP388_DEFAULT_ADDRESS);
 
-        /* Setup */
-        auto [OSR_REG, ODR_REG, PWR_CTRL_REG] = read_registers<uint8_t, 3>(0x1B);
-        write_reg(0x1B, PWR_CTRL_REG | 0b00110011);
-        write_reg(0x1C, OSR_REG | 0b00000010);
-        write_reg(0x1D, ODR_REG | 0b00000100);
-    };
+    bmp388_odr_t get_odr();
 
-    float read_pressure() {
-        auto [t0, t1, t2] = read_registers<uint8_t, 3>(0x04);
-        int pressure = ((int)t0 << 16) + ((int)t1 << 8) + ((int)t2);
-        return (float)pressure;
-    }
+    void set_odr(bmp388_odr_t odr);
+
+    bmp388_oversampling_t get_pressure_oversampling();
+
+    void set_pressure_oversampling(bmp388_oversampling_t oversampling);
+
+    bmp388_oversampling_t get_temperature_oversampling();
+
+    void set_temperature_oversampling(bmp388_oversampling_t oversampling);
+
+    float get_temperature();
+
+    float get_pressure();
 
 private:
+    uint16_t m_t1;
+    uint16_t m_t2;
+    int8_t m_t3;
+    int16_t m_p1;
+    int16_t m_p2;
+    int8_t m_p3;
+    int8_t m_p4;
+    uint16_t m_p5;
+    uint16_t m_p6;
+    int8_t m_p7;
+    int8_t m_p8;
+    int16_t m_p9;
+    int8_t m_p10;
+    int8_t m_p11;
+    int64_t m_t_fine;
 
+    int64_t _comp_temperature(uint32_t data);
+
+    int64_t _comp_pressure(uint32_t data);
 };
 
-#endif // ifndef EFC_HCSR04_HPP
+#endif // ifndef EFC_BMP388_HPP
